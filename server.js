f8ed54e21a73cf461eb0c473db0b31d96d507ba3
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-06 16:46:37 -0600

    filtered animals by multiple personality traits

--- server.js
+++ server.js
@@ -11,10 +11,59 @@ const app = express();
 // require the data for route
 const { animals } = require('./data/animals');
 
+// This function will take in req.query as an argument and filter through the animals accordingly, returning the new filtered array
+function filterByQuery(query, animalsArray) {
+
+    let personalityTraitsArray = [];
+
+    // Note that we save the animalsArray as filteredResults here:
+    let filteredResults = animalsArray;
+
+    if (query.personalityTraits) {
+        // Save personalityTraits as a dedicated array no matter if it's a string or an array already
+        // If personalityTraits is a string, place it into a new array and save.
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // Loop through each trait in the personalityTraits array:
+        // We're revising the filteredResults array for each trait that we loop through with .forEach(). Each iteration revises filteredResults so that it only contains animals that possess the indicated trait. At the end of the .forEach() loop, we'll have a filteredResults array that only contains animals that have all of the traits we're targeting.
+        personalityTraitsArray.forEach(trait => {
+            // Check the trait against each animal in the filteredResults array.
+            // Remember, it is initially a copy of the animalsArray, but here we're updating it for each trait in the .forEach() loop.
+            // For each trait being targeted by the filter, the filteredResults array will then contain only the entries that contain the trait, so at the end we'll have an array of animals that have every one of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
+            // animal.personalityTraits.indexOf(trait) !== -1 
+            // indexOf() array method, returns the 1st index # @ which a given element can be found in the array --> so it returns the index #
+            // returns -1 if it is ot present 
+            // so for each animal, return the animal if the indexOf that specific trait is not -1 (meaning if that trait exists in that animal)
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+         filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+
+    // return the filtered results:
+    return filteredResults;
+}
+
 // route created that front-end can request data from
 app.get('/api/animals', (req, res) => {
+
     let results = animals;
-    console.log(req.query)
+
+    // if there is a query parameter, then filter by it
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+
     res.json(results);
 });
 // the get() method requires two arguments:
