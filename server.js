de3c2c240ad81e1ac0d1bac9aa5dbd00748572bd
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-06 15:50:49 -0600

    tested req (request) object query property!

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ const { animals } = require('./data/animals');
 
 // route created that front-end can request data from
 app.get('/api/animals', (req, res) => {
-    res.json(animals);
+    let results = animals;
+    console.log(req.query)
+    res.json(results);
 });
 // the get() method requires two arguments:
 // 1st is a string that describes the route the client will have to fetch from && 2nd is a callback function that will execute every time that route is accessed with a GET request
 // changed from send to json --> so that the client knows it's receiving json (this changes the headers) --> and to send json data
-
+// res = response --> has json() method
+// req = request --> has query property --> it gets all the query parameters after ? and turns it into JSON
 
 
 // Now we just need to use one method to make our server listen
